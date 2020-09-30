cbddb407e3dd95fe9b84c141653438f5e4935cac
Hussain <md.afsar.hussain@sap.com>
2020-09-30 10:28:27 +0530

    updating concepts

--- ToolsAndFrameworks/JQuery/GrabbingTableData/dataGrabber.js
+++ ToolsAndFrameworks/JQuery/GrabbingTableData/dataGrabber.js
@@ -1,23 +1,26 @@
-//to be un in console of for seeing llive results -> Concepts\AJAX\Populating Table\table.html
+// to be un in console of for seeing llive results -> Concepts\AJAX\Populating Table\table.html
 
-var fun = function(){var myRows = [];
-    var $headers = $("th");
-    var $rows = $("tbody tr").each(function(index) {
-      $cells = $(this).find("td");
-      myRows[index] = {};
-      $cells.each(function(cellIndex) {
-        myRows[index][$($headers[cellIndex]).html()] = $(this).html();
-      });    
-    });
-    
-    // Let's put this in the object like you want and convert to JSON (Note: jQuery will also do this for you on the Ajax request)
-    var myObj = {};
-    myObj.myrows = myRows;
-    // console.log(JSON.stringify(myObj));
-    return myObj;
-}
+const fun = () => {
+	const myRows = [];
+	const $headers = $("th")(
+		$("tbody tr").each((index) => {
+			// eslint-disable-next-line no-undef
+			const cells = $(this).find("td");
+			myRows[index] = {};
+			cells.each((cellIndex) => {
+				myRows[index][$($headers[cellIndex]).html()] = $(this).html();
+			});
+		})
+	)();
+
+	// Let's put this in the object like you want and convert to JSON
+	// (Note: jQuery will also do this for you on the Ajax request)
+
+	const myObj = {};
+	myObj.myrows = myRows;
+	// console.log(JSON.stringify(myObj));
+	return myObj;
+};
 
 // additional console commands
 console.table(fun().myrows);
-
-
